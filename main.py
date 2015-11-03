9b9382ad0487389ca5ad9b1b32c80561c08a6732
CodeMeow <codemeow@yahoo.com>
2015-11-03 11:20:35 +0800

    log

--- main.py
+++ main.py
@@ -3,14 +3,19 @@
 # -*- coding:utf-8 -*- 
 
 import urllib2
+import logging
+import logging.handlers
 
-import sys
+import sys, os, re
 reload(sys)
 sys.setdefaultencoding('utf8')
+
+
 
 WEATHER_URI = 'http://vehiclenet-python-0-codemeow.myalauda.cn/carlink/weather/findWeather.htm?city=%s'
 
 def fetch_weather(city_name):
+    logger = logging.getLogger('attacker')
     print 'Try to fetch the weather in %s' % city_name
     try:
         #import pdb
@@ -20,10 +25,14 @@ def fetch_weather(city_name):
         response = urllib2.urlopen(WEATHER_URI % city_encode_name)
         content = response.read()
         if content == str(201) or content == str(501):
-            print 'Return error code: %s (%s)' % (content, city_name)
+            errorStr = 'Return error code: %s (%s)' % (content, city_name)
+            logger.error(errorStr)
+            print errorStr
         return 1
     except urllib2.HTTPError, e:
-        print 'Error: %s (%s)' % (e.code, city_name)
+        errorStr = 'Error: %s (%s)' % (e.code, city_name)
+        logger.error(errorStr)
+        print errorStr
     finally:
         return 0
 
@@ -34,6 +43,20 @@ if __name__ == '__main__':
     f = open('city_name.txt', 'r')
     i_total = 0
     i_success = 0
+
+    logdir = 'logs'
+    if not os.path.exists(logdir):
+        os.makedirs(logdir)
+    fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'
+    formatter = logging.Formatter(fmt)
+    handler = logging.handlers.TimedRotatingFileHandler(
+        '%s/logging' % logdir, 'H', 24, 0)
+    handler.suffix = '%Y%m%d%H%M%S.log'
+    handler.extMatch = re.compile(r'^\d{4}\d{2}\d{2}\d{2}\d{2}\d{2}')
+    handler.setFormatter(formatter)
+    logger = logging.getLogger('attacker')
+    logger.addHandler(handler)
+
     for line in f.readlines():
         line = line.strip()
         line = line.decode('unicode_escape')
